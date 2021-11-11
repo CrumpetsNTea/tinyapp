008811db9cdcfba966f4904509b37ee21d9d8bfd
CrumpetsNTea <t.botell@gmail.com>
2021-11-11 17:55:16 -0500

    A LOT of debugging after making the change to using encrypted cookies - had to replace all old syntax, and then ensure was using correct new syntax. quite a few errors popped up regarding how I was using the res.cookie and req.cookies and how that changed with req.session. Eventually got it all sorted out though. Did a lot of debugging regarding other things that weren't quite polished with my app either, fixed those up and all working now, tried to break the app and didn't get any errors so seems good to me.

--- express_server.js
+++ express_server.js
@@ -3,6 +3,12 @@ const app = express();
 app.set('view engine', 'ejs');
 const PORT = 8080; // default port 8080
 
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
+
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
@@ -22,25 +28,27 @@ const generateRandomString = () => {
 const checkEmail = (users, email) => {
   for (let user in users) {
     if (users[user].email === email) {
-      return users[user];
+      return true;
     }
   }
+  return false;
 };
 
-//HELPER FUNCTION FOR CHECKING PASSWORD IN DATABASE
-const checkPassword = (users, password) => {
+//HELPER FUNCTION GET USER NAME BY EMAIL - CAN USE IN LOGIN
+const getUserID = function(users, email) {
   for (let user in users) {
-    if (bcrypt.compareSync(password, users[user].password)) {
+    if (users[user].email === email) {
       return users[user];
     }
   }
+  return false;
 };
 
 //HELPER FUNCTION FOR USERS ONLY BEING ABLE TO SEE URLS TIED TO THEIR ACCOUNT
-const urlsForUser = function(id, urlDatabase) {
+const userURLs = function(id, urlDatabase) {
   const userURLs = {};
   for (let shortURL in urlDatabase) {
-    if (id === urlDatabase[shortURL].userID) {
+    if (urlDatabase[shortURL].userID === id) {
       userURLs[shortURL] = urlDatabase[shortURL];
     }
   }
@@ -51,11 +59,11 @@ const urlsForUser = function(id, urlDatabase) {
 const urlDatabase = {
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
-    userID: "pontiac@bandit.com"
+    userID: "pontiacbandit"
   },
   "9sm5xK": {
     longURL: "http://www.google.com",
-    userID: "pontiac"
+    userID: "userRandomID"
   },
 
 };
@@ -72,7 +80,7 @@ let users = {
     email: "user2@example.com",
     password: bcrypt.hashSync("funk", 10)
   },
-  "Doug Judy": {
+  "pontiacbandit": {
     id: "pontiacbandit",
     email: "pontiac@bandit.com",
     password: bcrypt.hashSync("jakeperalta", 10)
@@ -89,31 +97,32 @@ app.listen(PORT, () => {
 //RENDER ROOT PAGE
 app.get('/', (req, res) => {
   const templateVars = {
-    userId: req.cookies["user_id"],
-    urls: urlsForUser(req.cookies["user_id"], urlDatabase)
+    userId: req.session.userID,
+    urls: userURLs(req.session.userID.id, urlDatabase)
   };
   if (!templateVars.userId) { //if there is no userId
     res.redirect('/login');
   }
+  res.redirect('/urls');
   res.render('urls_index', templateVars); //'urls_index' is the name of the template we are passing our templateVars object to
 });
 
 //RENDER URLS/ROOT PAGE
 app.get('/urls', (req, res) => {
-  const templateVars = {
-    userId: req.cookies["user_id"],
-    urls: urlsForUser(req.cookies["user_id"].email, urlDatabase)
+  let templateVars = {
+    userId: req.session.userID,
   };
-  if (!templateVars.userId) { //if there is no userId
+  if (!templateVars.userId) { //if there is no userId - was getting an error if the userUrls function was inside templateVars at the top of the function defintion (cannot read .id of undefined - when there was no user logged in), so had to move it so if it passes the conditional for if a user is logged in then templateVars includes the userURLs function, if not then it doesn't and the error won't fire
     res.redirect('/login');//401 unauthorized
   }
+  templateVars = { userId: req.session.userID, urls: userURLs(req.session.userID.id, urlDatabase) };
   res.render('urls_index', templateVars); //'urls_index' is the name of the template we are passing our templateVars object to
 });
 
 //RENDER NEW URL PAGE
 app.get('/urls/new', (req, res) => {
   const templateVars = {
-    userId: req.cookies["user_id"]
+    userId: req.session.userID
   };
   if (!templateVars.userId) { //if there is no userId
     res.redirect('/login');
@@ -126,19 +135,29 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL, //using the shortURL
     longURL: urlDatabase[req.params.shortURL].longURL,
-    userId: req.cookies["user_id"]
+    userId: req.session.userID
   };
-  if (!urlsForUser(req.cookies["user_id"].email, urlDatabase)[req.params.shortURL]) {
-    return res.send("Sorry, you do not have access to this URL").status(401);
-  }
   if (!templateVars.userId) { //if there is no userId
     return res.send('Must be logged in to be able to see URLs').status(401); //redirect user because they have to be logged in or have an account to use the functions
   }
-  if (urlDatabase[req.params.shortURL] === undefined) { //if url does not exist
+  if (urlDatabase[templateVars.shortURL] === undefined) { //if url does not exist
     console.log("User tried inputting invalid Short URL"); //lets server know too
     return res.send("Invalid Short URL").status(400); //then tells the user and they can go back and try again
   } //otherwise things go ahead as per usual - it will pass if :shortURL exists in urldatabase and will continue correctly
+
+  if (!req.session.userID.id === urlDatabase[templateVars.shortURL].userID) {
+    return res.send("Sorry, you do not have access to this URL").status(401);
+  }
   res.render("urls_show", templateVars); //passes both to urls_show template and then sends the HTML to the browser
+});
+
+app.post('/urls/:shortURL', (req, res) => {
+  if (!req.session.userID) {
+    return res.status(404).send();
+  } else if (urlDatabase[req.params].userID !== req.session.userID) {
+    return res.status(404).send();
+  }
+  res.redirect('/urls/:shortURL');
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -161,13 +180,19 @@ app.get('/urls.json', (req, res) => {
 // ADD NEW URL
 app.post("/urls", (req, res) => {
   const templateVars = {
-    userId: req.cookies["user_id"]
+    userId: req.session.userID
   };
   if (!templateVars.userId) { //if there is no userId
     return res.send('Must be logged in to be able to ADD a new URL').status(400); //redirect user because they have to be logged in or have an account to use the functions
   }
+  if (req.body.longURL === undefined) {
+    return res.send('No URL Input');
+  }
   let newShortURL = generateRandomString();
-  urlDatabase[newShortURL] = {longURL: req.body.longURL, userID: req.cookies["user_id"].email};
+  urlDatabase[newShortURL] = {
+    longURL: req.body.longURL,
+    userID: req.session.userID.id,
+  };
   console.log(urlDatabase);
   res.redirect(`/urls/${newShortURL}`);
 });
@@ -175,34 +200,34 @@ app.post("/urls", (req, res) => {
 //DELETE URL
 app.post('/urls/:shortURL/delete', (req,res) => {
   const templateVars = {
-    userId: req.cookies["user_id"].email
+    shortURL: req.params.shortURL, //using the shortURL
+    longURL: urlDatabase[req.params.shortURL].longURL,
+    userId: req.session.userID
   };
   if (!templateVars.userId) { //if there is no userId
     return res.send('Must be logged in to be able to DELETE a URL').status(400); //redirect user because they have to be logged in or have an account to use the functions
   }
-  const shortURL = req.params.shortURL;
-  if (!urlsForUser(req.cookies["user_id"].email, urlDatabase)[shortURL]) {//if user does not own URL
+  if (urlDatabase[req.params.shortURL].userID !== req.session.userID.id) { //if user does not own URL
     return res.send("You do not have access to that URL").status(400);
   }
-  delete urlDatabase[shortURL];
+  delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
 
 //UPDATE/EDIT URL
 app.post('/urls/:shortURL/update', (req, res) => {
   const templateVars = {
-    userId: req.cookies["user_id"]
+    userId: req.session.userID
   };
   if (!templateVars.userId) { //if there is no userId
     return res.send('Must be logged in to be able to edit or update URL').status(400); //redirect user because they have to be logged in or have an account to use the functions
   }
-  const shortURL = req.params.shortURL; //set a variable for the shortURL so it's easier
   const updatedLongURL = req.body.longURL; //set a variable so it's easier
-  if (!urlsForUser(req.cookies["user_id"].email, urlDatabase)[shortURL]) { //if user does not own URL
+  if (urlDatabase[req.params.shortURL].userID !== req.session.userID.id) { //if user does not own URL
     return res.send("You do not have access to that URL").status(400);
   }
-  if (urlDatabase[req.params.shortURL].userID === templateVars.userId.email)
-    urlDatabase[shortURL].longURL = updatedLongURL; //longURL in the database now equals the updated URL
+  if (urlDatabase[req.params.shortURL].userID === req.session.userID.id)
+    urlDatabase[req.params.shortURL].longURL = updatedLongURL; //longURL in the database now equals the updated URL
   console.log("Updated URL"); //lets server-side know that a URL was updated successfully
   res.redirect('/urls');
 });
@@ -210,49 +235,48 @@ app.post('/urls/:shortURL/update', (req, res) => {
 //LOGIN PAGE
 app.get('/login', (req, res) => {
   const templateVars = {
-    userId: req.cookies["user_id"]
+    userId: req.session.userID
   };
   res.render('urls_login', templateVars); //takes user to Create TinyURL page
 });
 
 //LOGIN FUNCTION
 app.post('/login', (req, res) => {
-  const userInfo = {//object to hold these variables so they can be passed to the header upon successsful login to display user email
-    userPassword: req.body.password,
-    email: req.body.email
-  };
-  if (!userInfo.userPassword || !userInfo.email) {//if the email and/or password fields are left empty
+  if (!req.body.password || !req.body.email) {//if the email and/or password fields are left empty
     return res.send("Email and/or Password fields left empty").status(400); //error code and let the user know
   }
-  if (!checkEmail(users, userInfo.email)) { //if the function checkEmail returns false then that means that the email is not registered
+  if (!checkEmail(users, req.body.email)) { //if the function checkEmail returns false then that means that the email is not registered
     return res.send("Oops! Looks like that email is not associated with an account").status(403); //error code and let the user know
   }
-  if (checkEmail(users, userInfo.email)) { //if the function checkEmail returns true then that means that the email is already registered
-    if (!checkPassword(users, userInfo.userPassword)) { //if checkPassword fails then it means the user input the wrong password
+  if (checkEmail(users, req.body.email)) { //if the function checkEmail returns true then that means that the email is already registered
+    const userID = getUserID(users, req.body.email);
+    console.log(userID.password);
+    if (!bcrypt.compareSync(req.body.password, userID.password)) { //if checkPassword fails then it means the user input the wrong password
       return res.send("Oops! Wrong Password").status(403);
     }
-    res.cookie("user_id", userInfo);
-    return res.redirect('/urls');
+    req.session.userID = userID;
+    console.log(req.session.userID);
+    res.redirect('/urls');
   }
 });
 
 //LOGOUT FUNCTION
 app.post('/logout', (req, res) => { //clears the corresponding user's cookie
-  res.clearCookie("user_id");
+  req.session = null;
   res.redirect('/login');
 });
 
 //RENDERS REGISTER PAGE
 app.get('/register', (req, res) => { //renders our register page with the header
   const templateVars = {
-    userId: req.cookies["user_id"]
+    userId: req.session.userID
   };
   res.render('urls_register', templateVars);
 });
 
 //REGISTER FUNCTION
 app.post('/register', (req, res) => {
-  let randomUserID = generateRandomString(); //creates random ID
+  const randomUserID = generateRandomString(); //creates random ID
   const userPassword = req.body.password; //variable for their password
   const userEmail = req.body.email; //variable for email
   if (!userPassword || !userEmail) {//if the email and/or password fields are left empty
@@ -267,7 +291,7 @@ app.post('/register', (req, res) => {
     "email": userEmail,
     "password": bcrypt.hashSync(userPassword, 10)
   };
-  console.log(users); //console.logs the database of users just so we can check that it is working properly
-  res.cookie("user_id", users[randomUserID]);
+  console.log(users[randomUserID]); //console.logs the database of users just so we can check that it is working properly
+  req.session.userID = users[randomUserID];
   res.redirect('/urls'); //redirect the user
 });
